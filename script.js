eb0739ca25e8ecd891a796e421cd0c9ea979f0ae
umaumax <umauma7083@gmail.com>
2020-12-05 16:30:21 +0900

    Update setting

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 $(function() {
-    var simplemde = new SimpleMDE({
-        element: document.getElementById("wiki_content"),
-        insertTexts: {
-            // override image insert button
-            image: ["<details>\n<summary>", "</summary>\n<!-- you must insert blank line -->\n\n</details>"],
-        },
-        renderingConfig: {
-            codeSyntaxHighlighting: false,
-        },
-        toolbar: ["code", "horizontal-rule", "table", "|", "preview", "side-by-side", "|",
-            {
-                name: "expand",
-                action: SimpleMDE.drawImage,
-                className: "fa fa-plus-square",
-                title: "Expand",
-            }
-        ]
-    });
+    setTimeout(
+        function() {
+            var simplemde = new SimpleMDE({
+                element: document.getElementById("wiki_content"),
+                insertTexts: {
+                    // override image insert button
+                    image: ["<details>\n<summary>", "</summary>\n<!-- you must insert blank line -->\n\n</details>"],
+                },
+                autofocus: true,
+                forceSync: true,
+                shortcuts: {
+                    drawImage: "", // default is "Cmd-Alt-i" same as developer tools
+                },
+                spellChecker: false,
+                renderingConfig: {
+                    codeSyntaxHighlighting: false,
+                },
+                toolbar: ["code", "horizontal-rule", "table", "|", "preview", "side-by-side", "|",
+                    {
+                        name: "expand",
+                        action: SimpleMDE.drawImage,
+                        className: "fa fa-plus-square",
+                        title: "Expand",
+                    }
+                ]
+            });
+        }, "500"
+    );
 });
